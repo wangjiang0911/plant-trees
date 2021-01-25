0c2b30331ad8956f8da05d282b2f6e44550b4611
River <river.wang@analyticservice.net>
2021-01-25 16:17:16 +0800

    frist commit

---
+++ src/main/java/com/river/planttrees/PlantTreesApplication.java
@@ -0,0 +1,13 @@
+package com.river.planttrees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantTreesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantTreesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/river/planttrees/PlantTreesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.river.planttrees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantTreesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
